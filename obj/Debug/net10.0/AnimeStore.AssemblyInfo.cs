651595f62d07c5bc3d6442ada7444729a346ee9c
michael.lewellen <michael.lewellen@snow.edu>
2026-09-08 10:41:41 -0600

    Docker Reads CSS now

--- obj/Debug/net10.0/AnimeStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/AnimeStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1b78595ea7b3f2ff20605c9263dba4dfa969f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fbf5b519ce1ef54d1408d48487779791ea99da")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
