f2408a9c2f04e37a42cb29cfecff579a8437df55
michael.lewellen <michael.lewellen@snow.edu>
2026-09-16 09:17:01 -0600

    Minimal

--- obj/Debug/net10.0/AnimeStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/AnimeStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bab749376e229092ea3872797e97f94004bc834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9207f0f1e53d37bd1e351b683dee3cd229e99d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
