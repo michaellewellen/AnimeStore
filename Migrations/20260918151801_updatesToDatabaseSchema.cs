a2458c82514aef3f38abaec31c0dc87fa89a971f
michael.lewellen <michael.lewellen@snow.edu>
2026-09-18 10:01:27 -0600

    Properly updated cart.razor

---
+++ Migrations/20260918151801_updatesToDatabaseSchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimeStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatesToDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CutomerId",
+                table: "Carts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CutomerId",
+                table: "Carts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+builder.Services.AddScoped<GuestCartService>();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
